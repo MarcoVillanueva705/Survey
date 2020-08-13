9248b06f7622d09e7590ccad0c10261e544428b7
marco villanueva <villanuevamarco98@gmail.com>
2020-08-12 19:55:07 -0600

    Add project files.

---
+++ Survey/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Survey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            var age = Console.ReadLine();
+
+            Console.WriteLine($"Your name is: { name }");
+            Console.WriteLine($"Your age is { age }");
+        }
+    }
+}
