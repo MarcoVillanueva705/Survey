389ef4714dc609ef801313e5930dbfc57e97c22b
marco villanueva <villanuevamarco98@gmail.com>
2020-08-19 20:42:55 -0600

    need while loop

--- Survey/Program.cs
+++ Survey/Program.cs
@@ -8,22 +8,32 @@ namespace Survey
         static void Main(string[] args)
         {
             Console.WriteLine("What is your name?");
-            //WriteTryAgain()
             var name = Console.ReadLine();
+
+            if (name == "")
+            {
+                writeTryAgain();
+            }
 
             Console.WriteLine("What is your age?");
             var age = Console.ReadLine();
 
+            if (age == "")
+            {
+                writeTryAgain();
+            }
+
             Console.WriteLine("What month were you born in?");
             var month = Console.ReadLine();
 
+            if (month == "")
+            {
+                writeTryAgain();
+            }
+
             Console.WriteLine($"Your name is: { name }");
             Console.WriteLine($"Your age is { age }");
-
-            if (month == "" || name == "" || age == "")
-            {
-                Console.WriteLine("Inavlid entry!");
-            }
+            Console.WriteLine($"Your month is { month }");
         }
         static void writeTryAgain()
         {
