f82928a746677036e709e291002ce69d2802424a
marco villanueva <villanuevamarco98@gmail.com>
2020-08-20 21:04:03 -0600

    added while loops

--- Survey/Program.cs
+++ Survey/Program.cs
@@ -7,33 +7,50 @@ namespace Survey
         //adding functions to exercise tomorrow
         static void Main(string[] args)
         {
+            var name = "";
+            var age = "";
+            var month = "";
+
+            while (name == "") { 
+
             Console.WriteLine("What is your name?");
-            var name = Console.ReadLine();
+            name = Console.ReadLine();
+
 
             if (name == "")
             {
                 writeTryAgain();
             }
+        }
+            while (age == "") { 
+            
 
-            Console.WriteLine("What is your age?");
-            var age = Console.ReadLine();
+                Console.WriteLine("What is your age?");
+                age = Console.ReadLine();
 
-            if (age == "")
-            {
-                writeTryAgain();
+
+                if (age == "")
+                {
+                    writeTryAgain();
+                }
             }
-
-            Console.WriteLine("What month were you born in?");
-            var month = Console.ReadLine();
-
-            if (month == "")
+            while (month == "")
             {
-                writeTryAgain();
+
+                Console.WriteLine("What month were you born in?");
+                month = Console.ReadLine();
+
+
+                if (month == "")
+                {
+                    writeTryAgain();
+                }
             }
+            
 
             Console.WriteLine($"Your name is: { name }");
             Console.WriteLine($"Your age is { age }");
-            Console.WriteLine($"Your month is { month }");
+            Console.WriteLine($"You were born in { month }");
         }
         static void writeTryAgain()
         {
